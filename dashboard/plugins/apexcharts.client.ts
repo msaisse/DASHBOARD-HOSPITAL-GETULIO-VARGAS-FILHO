9e7a470a1c61eb27a5125f90fed320ea03841fe7
Manoel Saisse <43208625+msaisse@users.noreply.github.com>
2025-12-30 20:48:28 -0300

    Update apexcharts.client.ts

--- dashboard/plugins/apexcharts.client.ts
+++ dashboard/plugins/apexcharts.client.ts
@@ -1,3 +1,8 @@
+#Habilita:
+#gráficos de evolução temporal
+#séries de tempo (tempo médio, volume)
+#visualização histórica dos deltas
+
 import VueApexCharts from 'vue3-apexcharts';
 
 export default defineNuxtPlugin((nuxtApp) => {
